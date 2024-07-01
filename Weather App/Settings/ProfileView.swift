d5ecde4630feaee3f052c91678206901a98d11bb
Soumik Bhattacharyya <soumikb1240@gmail.com>
2024-07-01 19:10:59 +0530

    Revised

--- Weather App/Settings/ProfileView.swift
+++ Weather App/Settings/ProfileView.swift
@@ -12,6 +12,7 @@ struct ProfileView: View {
     @State private var name: String = "Soumik"
     @ObservedObject var viewModel = LoginViewModel()
     @Environment(\.presentationMode) var presentationMode
+    @State private var isDarkMode = false
     
     var body: some View {
         VStack {
@@ -38,10 +39,21 @@ struct ProfileView: View {
                 .font(.headline)
                 .foregroundColor(.gray)
                 .padding(.top, 5)
+                .padding(.bottom, 15)
             
+           
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Preference:")
+                
+                Picker("Theme", selection: $isDarkMode) {
+                    Text("Light").tag(false)
+                    Text("Dark").tag(true)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
+            }
             Spacer()
-            
-            // Logout Button
             Button(action: {
                 print("Logout Button tapped!")
                 logout()
